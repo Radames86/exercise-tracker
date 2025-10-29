7b38ef8b224ac8973d6035e5b5224dcd41da8aed
Radames Rivera <rrivera@perseverenow.org>
2025-10-29 16:34:44 -0500

    second

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+});
