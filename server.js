0690234af50f31f522950a56cfea94fd25cb9cb4
Radames Rivera <rrivera@perseverenow.org>
2025-10-30 10:30:45 -0500

    third

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('connected to MongoDB');
+})
+.catch(err => {
+    console.error(' MongoDB connection error:', err.message);
+});
 
 const app = express();
 
